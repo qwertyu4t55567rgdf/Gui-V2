b2a7b92192baf55041230817c6ea41c60e29c839
qwertyu4t55567rgdf <153318778+qwertyu4t55567rgdf@users.noreply.github.com>
2024-09-25 00:07:39 +0300

    Update RvvzFuckYou.lua

--- RvvzFuckYou.lua
+++ RvvzFuckYou.lua
@@ -22,11 +22,11 @@ local function fullbrightL(value)
 end
 
 local function open_doorsL()
-        remotes.open_doorsRun = run.RenderStepped:Connect(function()
+    remotes.open_doorsRun = run.RenderStepped:Connect(function()
         for _, i in pairs(game.Workspace.Map.Doors:GetChildren()) do
-            if (game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position - v:FindFirstChild("DoorBase").Position).Magnitude <= 20 then
-                if v:FindFirstChild("Values"):FindFirstChild("Locked").Value == true then
-                    v:FindFirstChild("Events"):FindFirstChild("Toggle"):FireServer("Unlock", v.Lock)
+            if (game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position - i:FindFirstChild("DoorBase").Position).Magnitude <= 20 then
+                if i:FindFirstChild("Values"):FindFirstChild("Locked").Value == true then
+                    i:FindFirstChild("Events"):FindFirstChild("Toggle"):FireServer("Unlock", i.Lock)
                 end
             end
         end
